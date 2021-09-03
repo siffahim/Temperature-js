692283c8f93d450c4768a18c7f81cb7f7c1f1cd2
siffahim <siffahim25@gmail.com>
2021-09-03 10:56:52 +0600

    add style

--- app.js
+++ app.js
@@ -7,23 +7,42 @@ const tempareture = () => {
         alert('please,write here')
 
     } else {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputFieldText}&appid=${'8a87d865049fdb358233a36db352b7f8'}`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputFieldText}&appid=${'8a87d865049fdb358233a36db352b7f8'}&units=metric`
     
         fetch(url).then(res => res.json()).then(json => displayWeather(json))
     }
-
-
-    
-
     inputField.value = '';
 }
 
-const displayWeather = data => {
-  console.log(data)
-    const weather = document.getElementById('weather');
-    weather.textContent = '';
+const searchWetaherText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayWeather = tempareture => {
+    console.log(tempareture.cod)
+    if (tempareture.cod == 404) {
+        document.getElementById('waring').style.display = 'block';
+        document.getElementById('weather').style.display = 'none';
+    }
+    else {
+        searchWetaherText('city', tempareture.name);
+        searchWetaherText('tempareture', tempareture.main.temp);
+        searchWetaherText('condition', tempareture.weather[0].main);
+        searchWetaherText('humidity', tempareture.main.humidity);
+        
+        const img = `http://openweathermap.org/img/wn/${tempareture.weather[0].icon}@2x.png`;
+        const imgIcon = document.getElementById('img-icon');
+        imgIcon.setAttribute('src', img)
+        
+        document.getElementById('waring').style.display = 'none';
+        document.getElementById('weather').style.display = 'block';
+    }
+
     
-    try {
+    //another way
+   /*  try {
+        const weather = document.getElementById('weather');
+        weather.textContent = '';
         const div = document.createElement('div');
         div.innerHTML = `
             <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" width="100px"/>
@@ -36,9 +55,9 @@ const displayWeather = data => {
         weather.appendChild(div)
         document.getElementById('waring').style.display = 'none';
     } catch (err) {
-        document.getElementById('waring').style.display = 'block';
+       
     }
-
+ */
 
 
 }
